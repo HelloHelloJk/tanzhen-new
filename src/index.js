ad01b022270584413c27b91aa85e48e05642cf95
HelloHelloJk <jeki.0924@gmail.com>
2026-09-20 11:38:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2271,15 +2271,14 @@ rm -f /tmp/cf_install.sh
             ${filterTagsHtml}
           </div>
 
-          <div class="global-stats" id="ajax-stats">
-            <div class="stats-row top-row">
-              <div class="g-item">
-                <div class="g-label"></div>
-                <div class="g-sub" style="text-align: center;">在线 <span style="color:#10b981">${globalOnline}</span> &nbsp; 离线 <span style="color:#ef4444">${globalOffline}</span></div>
-              </div>
-              
-              <div class="g-item" style="border-left: 3px solid #f59e0b; padding-left:15px; border-radius: 0;">
-              </div>
+         <div class="global-stats" id="ajax-stats">
+  <div class="stats-row top-row" style="display: flex !important; justify-content: center !important; grid-template-columns: none !important;">
+    <div class="g-item" style="border: none !important; padding: 0 !important; width: 100% !important; text-align: center !important;">
+      <div class="g-sub" style="text-align: center !important; font-size: 14px;">
+        在线 <span style="color:#10b981; font-weight: bold;">${globalOnline}</span> &nbsp;&nbsp;&nbsp;&nbsp; 离线 <span style="color:#ef4444; font-weight: bold;">${globalOffline}</span>
+      </div>
+    </div>
+  </div>
 
               <div class="g-item">
               </div>
